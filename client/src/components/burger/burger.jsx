d1024d7398abe7287cb2e379d69cf45b14f5c828
Alexandr Kolomytski <alexandr_freelance@mail.ru>
2022-04-30 09:16:25 +1000

    .

--- client/src/components/burger/burger.jsx
+++ client/src/components/burger/burger.jsx
@@ -2,37 +2,104 @@ import React from 'react';
 import './burger.css'
 
 const Burger = () => {
-  
-  
+  "use strict"
+  const isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+      return (
+        isMobile.Android() ||
+        isMobile.BlackBerry() ||
+        isMobile.iOS() ||
+        isMobile.Opera() ||
+        isMobile.Windows());
+    }
+  };
+  if (isMobile.any()) {
+    document.body.classList.add('_touch');
+  } else {
+    document.body.classList.add('_pc');
+  };
+  // Меню бургер
+  const iconMenu = document.querySelector('.menu__icon')
+  const menuBody = document.querySelector('.menu__body')
+  if (iconMenu) {
+    iconMenu.addEventListener('click', function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+    })
+  }
+  // Прокрутка при клике
+  const menuLinks = Array.from(document.querySelectorAll('.menu__link[data-goto]'));
+  if (menuLinks.length) {
+    menuLinks.forEach(elem => {
+      elem.addEventListener('click', onMenuClick)
+    })
+    function onMenuClick(e) {
+      const menuLink = e.target;
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('header').offsetHeight;
+
+        if (iconMenu.classList.contains('_active')) {
+          document.body.classList.remove('_lock');
+          iconMenu.classList.remove('_active');
+          menuBody.classList.remove('_active');
+        }
+        window.scrollTo({
+          top: gotoBlockValue,
+          behavior: "smooth"
+        });
+        e.preventDefault();
+      }
+    }
+  };
+
   return (
-    <div class="header__menu menu">
-      <div class="menu__icon">
-        <span></span>
+    <header>
+      <div className="header__menu menu">
+        <div className="menu__icon">
+          <span></span>
+        </div>
+        <nav className="menu__body">
+          <ul className="menu__list">
+            <li><a data-goto=".page__section-1" href="" className="menu__link">Раздел №1</a></li>
+            <li><a data-goto=".page__section-2" href="" className="menu__link">Раздел №2</a></li>
+            <li><a data-goto=".page__section-3" href="" className="menu__link">Раздел №3</a></li>
+            <li>
+              <a href="" className="menu__link">Страница 1</a>
+              <span className="menu__arrow"></span>
+              <ul className="menu__sub-list">
+                <li>
+                  <a href="" className="menu__sub-link">Раздел Страницы 1</a>
+                </li>
+                <li>
+                  <a href="" className="menu__sub-link">Раздел Страницы 2</a>
+                </li>
+                <li>
+                  <a href="" className="menu__sub-link">Раздел Страницы 3</a>
+                </li>
+              </ul>
+            </li>
+            <li><a href="" className="menu__link">Страница 2</a></li>
+          </ul>
+        </nav>
       </div>
-      <nav class="menu__body">
-        <ul class="menu__list">
-          <li><a data-goto=".page__section-1" href="" class="menu__link">Раздел №1</a></li>
-          <li><a data-goto=".page__section-2" href="" class="menu__link">Раздел №2</a></li>
-          <li><a data-goto=".page__section-3" href="" class="menu__link">Раздел №3</a></li>
-          <li>
-            <a href="" class="menu__link">Страница 1</a>
-            <span class="menu__arrow"></span>
-            <ul class="menu__sub-list">
-              <li>
-                <a href="" class="menu__sub-link">Раздел Страницы 1</a>
-              </li>
-              <li>
-                <a href="" class="menu__sub-link">Раздел Страницы 2</a>
-              </li>
-              <li>
-                <a href="" class="menu__sub-link">Раздел Страницы 3</a>
-              </li>
-            </ul>
-          </li>
-          <li><a href="" class="menu__link">Страница 2</a></li>
-        </ul>
-      </nav>
-    </div>
+    </header>
   )
 };
 
